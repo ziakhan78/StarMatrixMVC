61e8397594237225e97815dddf4107c7afde8847
Zia <ziakhan78@gmail.com>
2019-11-30 16:46:58 +0530

    30-11-2019

--- Controllers/RecyclingController.cs
+++ Controllers/RecyclingController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using StarMatrix.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -43,6 +47,79 @@ namespace StarMatrix.Controllers
         public ActionResult GreenRecycling()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Recycling recycling)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(recycling);
+                return View("GetAQuote");
+            }
+
+            return View("GetAQuote");
+        }
+
+        [AllowAnonymous]
+        [NonAction]
+        public void SendEmail(Recycling obj)
+        {
+            try
+            {
+                string ReadFileName = "";
+                MailMessage mail = new MailMessage();
+
+               // mail.To.Add(obj.Email);
+                mail.To.Add("mail@starmatrixltd.com");
+                
+                mail.Bcc.Add("zia@goradiainfotech.com");
+                //mail.Bcc.Add("finefixengineering@gmail.com");
+                mail.From = new MailAddress("mail@starmatrixltd.com");
+                //mail.Subject = obj.Subject;               
+
+                mail.Subject = "Recycling - Get a Quote";
+
+                mail.IsBodyHtml = true;
+                string PathVal = Server.MapPath("~");
+
+                ReadFileName = PathVal + "/mail/Recycling_GetAQuote.htm";
+
+                string strMessage = "";
+                StreamReader sr1 = new StreamReader(ReadFileName);
+
+                strMessage = sr1.ReadToEnd();
+
+                strMessage = strMessage.Replace("XXXTypeofUnit", obj.Typeofunit);
+                strMessage = strMessage.Replace("XXXEstimatedLigthship", obj.EstimatedLigthship);
+                strMessage = strMessage.Replace("XXXLocation", obj.Location);
+                strMessage = strMessage.Replace("XXXLaidUp", obj.LaidUp);
+
+                strMessage = strMessage.Replace("XXXStatus", obj.Status);
+                strMessage = strMessage.Replace("XXXDeliveryLaycan", obj.DeliveryLaycan);
+                strMessage = strMessage.Replace("XXXDimensions", obj.Dimensions);
+                strMessage = strMessage.Replace("XXXHKC", obj.HKC);
+
+                mail.Body = strMessage;
+                sr1.Close();
+
+                SmtpClient emailClient = new SmtpClient();
+                emailClient.Credentials = new NetworkCredential("mail@starmatrixltd.com", "d&m3#BR8");
+                emailClient.Port = 587;
+                emailClient.Host = "smtp.gmail.com";
+                emailClient.EnableSsl = true;
+                emailClient.Send(mail);
+
+                ModelState.Clear();
+                ViewBag.Message = "Your enquiry has been submitted successfully.";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrMessage = "Problem while sending email, Please check details.";
+            }
+
         }
     }
 }
--- Controllers/TowageController.cs
+++ Controllers/TowageController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.Net.Mail;
+using System.IO;
+using System.Net;
 
 namespace StarMatrix.Controllers
 {
@@ -31,7 +34,7 @@ namespace StarMatrix.Controllers
         }
 
         [Route("Towage/Availability")]
-        public  ActionResult Availability()
+        public ActionResult Availability()
         {
             var shipLocations = db.ShipLocations.Include(s => s.Tugs);
             return View(shipLocations.ToList());
@@ -47,6 +50,78 @@ namespace StarMatrix.Controllers
         public ActionResult JobsUndertaken()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Towage towage)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(towage);
+                return View("GetAQuote");
+            }
+
+            return View("GetAQuote");
+        }
+
+        [AllowAnonymous]
+        [NonAction]
+        public void SendEmail(Towage obj)
+        {
+            try
+            {
+                string ReadFileName = "";
+                MailMessage mail = new MailMessage();
+
+                //mail.To.Add(obj.Email);
+                mail.To.Add("mail@starmatrixltd.com");
+                mail.Bcc.Add("zia@goradiainfotech.com");
+                //mail.Bcc.Add("finefixengineering@gmail.com");
+                mail.From = new MailAddress("mail@starmatrixltd.com");
+                //mail.Subject = obj.Subject;               
+
+                mail.Subject = "Towage - Get a Quote";
+
+                mail.IsBodyHtml = true;
+                string PathVal = Server.MapPath("~");
+
+                ReadFileName = PathVal + "/mail/Towage_GetAQuote.htm";
+
+                string strMessage = "";
+                StreamReader sr1 = new StreamReader(ReadFileName);
+
+                strMessage = sr1.ReadToEnd();
+
+                strMessage = strMessage.Replace("XXXDeparturePort", obj.DeparturePort);
+                strMessage = strMessage.Replace("XXXDestinationPort", obj.DestinationPort);
+                strMessage = strMessage.Replace("XXXEstimatedDisplacement", obj.EstimatedDisplacement);
+                strMessage = strMessage.Replace("XXXTowingDraft", obj.TowingDraft);
+                strMessage = strMessage.Replace("XXXDimensions", obj.Dimensions);
+                strMessage = strMessage.Replace("XXXEstimatedLaycan", obj.EstimatedLaycan);
+                strMessage = strMessage.Replace("XXXTypeofTow", obj.TypeofTow);
+                strMessage = strMessage.Replace("XXXMinimumBPRequirements", obj.MinimumBPRequirements);
+                
+
+                mail.Body = strMessage;
+                sr1.Close();
+
+                SmtpClient emailClient = new SmtpClient();
+                emailClient.Credentials = new NetworkCredential("mail@starmatrixltd.com", "d&m3#BR8");
+                emailClient.Port = 587;
+                emailClient.Host = "smtp.gmail.com";
+                emailClient.EnableSsl = true;
+                emailClient.Send(mail);
+
+                ModelState.Clear();
+                ViewBag.Message = "Your enquiry has been submitted successfully.";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrMessage = "Problem while sending email, Please check details.";
+            }
+
         }
     }
 }
